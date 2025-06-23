6bcca155789b09ce74508614f1ada51370f1197a
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-06-23 15:40:45 +0200

    Update IBannerService to support subsites

--- UMT.IServices/Banner/IBannerService.cs
+++ UMT.IServices/Banner/IBannerService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,8 @@ namespace UMT.IServices.Banner
 {
     public interface IBannerService
     {
-        void CreateCustomBanner(string siteUrl, string bannerJs);
-        void CreateAutoRedirectNotification(string siteUrl, string redirectUrl = null, int? countdownSeconds = null, string message = null);
-        void RemoveAllOptions(string siteUrl);
+        void CreateCustomBanner(string siteUrl, string bannerJs, bool applyToSubsites = false);
+        void CreateAutoRedirectNotification(string siteUrl, string redirectUrl = null, int? countdownSeconds = null, string message = null, bool applyToSubsites = false);
+        void RemoveAllOptions(string siteUrl, bool removeFromSubsites = false);
     }
 }
